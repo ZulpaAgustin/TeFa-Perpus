1565637f9a73ece2dd07d17170f61f3e7b5744d4
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2024-02-29 15:38:38 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                <img src="~/assets/img/HH.jpg" alt="" />
+            </div>
+        <div class="col-lg-6 mt-5">
+            <h2 class="text-center my-4">ISI BUKU KUNJUNGAN</h2>
+            <form class="mt-5">
+                <div class="mb-4">
+                    <input type="text" class="form-control form-control-lg rounded-5" placeholder="Nama" />
+                </div>
+                <div class="mb-4">
+                    <select class="form-control form-control-lg form-select rounded-5">
+                        <option value="">Keanggotaan</option>
+                        <option value="Siswa">Siswa</option>
+                        <option value="Guru">Guru</option>
+                        <option value="Staf">Staf</option>
+                        <option value="Umum">Umum</option>
+                    </select>
+                </div>
+                <div class="mb-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <select class="form-control form-control-lg form-select rounded-5 mb-2">
+                                <option value="">Tingkat</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <select class="form-control form-control-lg form-select rounded-5 mb-2">
+                                <option value="">Jurusan</option>
+                                <option value="PPLG">PPLG</option>
+                                <option value="TJKT">TJKT</option>
+                                <option value="TBSM">TBSM</option>
+                                <option value="DKV">DKV</option>
+                                <option value="TOI">TOI</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <select class="form-control form-control-lg form-select rounded-5 mb-2">
+                                <option value="">Kelas</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <select class="form-control form-control-lg form-select rounded-5">
+                        <option value="">Keperluan</option>
+                        <option value="Baca">Membaca Buku</option>
+                        <option value="Pinjam">Pinjam Buku</option>
+                        <option value="Kembalikan">Kembalikan Buku</option>
+                    </select>
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <nuxt-link to="/pengunjung/">
+                        <button type="submit" class="btn btn-light btn-lg rounded-5 px-5 me-md-2 mt-4">Kirim</button>
+                    </nuxt-link>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.container-fluid {
+    background-color: #4377de;
+}
+h2 {
+    color: #fff;
+}
+</style>
---
+++ pages/new-book/new1.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="icon mt-4">
+                    <nuxt-link to="/pengunjung/menu">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="57" height="40" fill="currentColor" class="bi bi-arrow-left-square-fill text-white" viewBox="0 0 16 16">
+                            <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1" />
+                        </svg>
+                    </nuxt-link>
+                    <h2 class="text-center my-4">The Sadness Winter in Korea</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4 text-white d-flex flex-column justify-content-center">
+                <h4>Detail Buku:</h4>
+                <h5>Pengarang: Rizki De</h5>
+                <h5>Jumlah Halaman: 462</h5>
+                <h5>Tanggal Terbit: 5 Juni 2012</h5>
+                <h5>Penerbit: C.V ANDI OFFSET</h5>
+            </div>
+            <div class="col-4 d-flex flex-column justify-content-center align-items-center">
+                <div class="mb-5">
+                    <div class="card-body">
+                        <img src="~/assets/img/korea.jpg" class="img-thumbnail" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 d-flex flex-column justify-content-center align-items-center">
+                <button type="button" class="btn btn-lg text-white mb-4" style="background-color: #123577; width: 200px">Meminjam</button>
+                <button type="button" class="btn btn-lg text-white mb-4" style="background-color: #123577; width: 200px">Membaca Buku</button>
+                <button type="button" class="btn btn-lg text-white mb-4" style="background-color: #123577; width: 200px">Rak 12</button>
+            </div>
+        </div>
+
+        <div class="row text-white">
+            <div class="col">
+                <h4>Deskripsi:</h4>
+                <h5>Kebanyakan orang pasti menyukai artis, tapi tidak dengan Park Yoona. Baginya mereka adalah makhluk bermuka dua yang selalu membuat dirinya naik darah. Niatnya untuk mencari inspirasi baru malah terganggu dengan kehadiran makhluk yang paling dibencinya itu. Mereka adalah artis tenar ikon baru Seoul, Ji sun, Jung Yong, dan Kevin.
+Di tengah situasi kacau balau itulah Yoona bertemu dengan lelaki misterius. Yoona selalu terhipnotis hingga sanggup melupakan masa lalunya yang kelabu tiap kali bertemu lelaki itu. Lalu diam-diam perasaan cinta lahir. Hanya saja Yoona belum bisa memastikan cinta itu untuk siapa. Apakah untuk Ji sun, Jung Yong, Kevin, atau lelaki misterius itu. Yoona hanya bisa pastikan dia menginginkan cerita cinta yang bahagia. Akankah Yoona mendapatkannya?
+                </h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container-fluid {
+    background-color: #4377de;
+}
+h2 {
+    color: #fff;
+}
+</style>
